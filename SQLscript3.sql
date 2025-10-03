b6791f1ddba1775d4401901e086f36398b3d6f7c
MOHAMED NASSIR <38281542+mohamedaffif@users.noreply.github.com>
2025-10-03 19:20:05 +0300

    Update SQLscript3.sql
    added advanced sql

--- SQLscript3.sql
+++ SQLscript3.sql
@@ -38,3 +38,95 @@ join products as pr on pr.pid = s.pid
 group by pr.Product
 order by 'Total Amount' desc
 limit 10;
+
+
+
+
+-- advanced sql 
+-- 1. view the table (note the missing dates)
+SELECT * FROM sales;
+-- 2. preview the finale results
+SELECT * FROM final_sales;
+
+-- 3. generate a series of dates [UNION, UNION ALL]
+SELECT '2025-01-01' dt
+UNION ALL
+SELECT '2025-01-02'
+UNION ALL
+SELECT '2025-01-02';
+
+-- 4. join with our original table [Subquery, left join, inner join]
+SELECT sq.dt, sales.num_sales 
+FROM 
+(SELECT '2025-01-01' dt
+UNION ALL
+SELECT '2025-01-02'
+UNION ALL
+SELECT '2025-01-03'
+UNION ALL
+SELECT '2025-01-04'
+UNION ALL
+SELECT '2025-01-05'
+UNION ALL
+SELECT '2025-01-06'
+UNION ALL
+SELECT '2025-01-07') AS sq
+ -- LEFT JOIN sales ON sq.dt = sales.dt
+INNER JOIN sales ON sq.dt = sales.dt
+;
+
+-- 5. Rewrite subquery as a CTE [CTE]
+WITH cte AS (SELECT '2025-01-01' dt
+			UNION ALL
+			SELECT '2025-01-02'
+			UNION ALL
+			SELECT '2025-01-03'
+			UNION ALL
+			SELECT '2025-01-04'
+			UNION ALL
+			SELECT '2025-01-05'
+			UNION ALL
+			SELECT '2025-01-06'
+			UNION ALL
+			SELECT '2025-01-07') 
+SELECT 		cte.dt, sales.num_sales
+ FROM 		cte LEFT JOIN sales ON cte.dt = sales.dt;
+ 
+ -- 6. rewrite CTE as a recursive CTE (Recursive CTE , Date Expression, Date function)
+ 
+ WITH RECURSIVE cte AS (SELECT CAST('2025-01-01' AS DATE) dt
+			UNION ALL
+			SELECT dt + INTERVAL 1 DAY  
+            FROM cte 
+            WHERE dt < CAST('2025-01-07' AS DATE)
+            )
+		
+SELECT 		cte.dt, sales.num_sales
+ FROM 		cte LEFT JOIN sales ON cte.dt = sales.dt;
+ 
+ -- 7 . fill in null values [NULL Function, Numeric Function]
+ WITH RECURSIVE cte AS (SELECT CAST('2025-01-01' AS DATE) dt
+			UNION ALL
+			SELECT dt + INTERVAL 1 DAY  
+            FROM cte 
+            WHERE dt < CAST('2025-01-07' AS DATE)
+            )
+		
+SELECT 		cte.dt, sales.num_sales,
+			COALESCE(sales.num_sales,0) AS sales_estimate,
+            COALESCE(sales.num_sales, ROUND((SELECT AVG(sales.num_sales) FROM sales),2)) AS sales_estimate_2
+ FROM 		cte LEFT JOIN sales ON cte.dt = sales.dt;
+ 
+-- 8. introduce window functions [WINDOW FUNCTIONS]
+SELECT dt, num_sales FROM sales;
+ 
+ 
+
+
+
+
+
+
+
+
+
